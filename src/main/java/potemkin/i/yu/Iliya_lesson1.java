dcff3a6b76c11f2f7212ff398db0dcad27e1d306
ilavio <ilavio777@gmail.com>
2021-03-23 10:21:49 +0400

    console output Hello World

---
+++ src/main/java/potemkin/i/yu/Iliya_lesson1.java
@@ -0,0 +1,15 @@
+package potemkin.i.yu;
+
+public class Iliya_lesson1 {
+	
+	public static void hello() {
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		
+		hello();
+
+	}
+
+}
